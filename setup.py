6daad252b3f3f6d8305754714b5ba0bca158377f
OllyPrice <op.oliverprice@gmail.com>
2018-09-18 15:27:39 +0100

    Setup version update

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='disagree',
-      version='0.1.0',
+      version='0.1.2',
       author='Oliver Price',
       author_email='op.oliverprice@gmail.com',
       url='https://github.com/o-P-o/annotations',
       packages=['disagree', 'disagree.test'],
+      include_package_data=True,
       scripts=['bin/plot_utils.py'],
       licence='LICENCE',
       description='Visual and statistical assessment of annotator agreements',
