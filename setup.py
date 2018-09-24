a2735764254a26e28e65b175575ff19499794586
OllyPrice <op.oliverprice@gmail.com>
2018-09-24 14:58:31 +0100

    Updated setup script

--- setup.py
+++ setup.py
@@ -1,19 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(name='disagree',
-      version='0.1.7',
+      version='0.1.11',
       author='Oliver Price',
       author_email='op.oliverprice@gmail.com',
       url='https://github.com/o-P-o/annotations',
       packages=['disagree', 'disagree.test'],
       include_package_data=True,
-      scripts=['bin/plot_utils.py'],
       licence='LICENCE',
       description='Visual and statistical assessment of annotator agreements',
       long_description=open('README.md').read(),
       install_requires=[
         'scipy >= 1.1.0',
         'tqdm >= 4.26.0',
-        'pandas >= 0.23.0',
-        'matplotlib >= 2.2.3']
+        'pandas >= 0.23.0']
      )
