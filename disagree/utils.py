97b432c98bea04a77e4860c38666278418d144f2
Oliver Price <35378674+o-P-o@users.noreply.github.com>
2020-04-18 14:50:13 +0100

    Remove use of .isnan in utils

--- disagree/utils.py
+++ disagree/utils.py
@@ -1,39 +1,22 @@
 import pandas as pd
 import numpy as np
-import math
-
-def move_to_end(lst):
-    # If there is a None in the list, move to the end. If there is a NaN
-    # then replace with None and move to the end.
-    new_list = []
-    for label in lst:
-        if not isinstance(label, str):
-            if float(label) == 0.0:
-                new_list.append(0.0)
-        if label:
-            if isinstance(label, str):
-                new_list.append(label)
-            else:
-                if not math.isnan(label):
-                    new_list.append(label)
-
-    return new_list + [None]
 
 def count_nans(lst):
     # Count the number of NaNs in a list.
     num_nans = 0
-    for x in lst:
-        if isinstance(x, str):
-            continue
-        if math.isnan(x):
+    null_list = list(pd.isnull(lst))
+    for is_null in null_list:
+        if is_null:
             num_nans += 1
 
     return num_nans
 
 def append_one_nan(old_lst, new_lst):
-    for x in old_lst:
-        if math.isnan(x):
-            new_lst.append(x)
+    old_null_list = list(pd.isnull(old_lst))
+    for i in range(len(old_null_list)):
+        is_null = old_null_list[i]
+        if is_null:
+            new_lst.append(old_lst[i])
             break
 
     return new_lst
@@ -45,50 +28,51 @@ def flexible_data(df):
             data_dict, dictionary connecting the original data point to its
                     converted number
     '''
-    all_data = []
+    all_data = [] # Generate a list of every label
     for idx, row in df.iterrows():
         all_data += list(row)
 
-    unique_data = list(set(all_data))
-    num_nans = count_nans(unique_data)
+    unique_data = list(set(all_data)) # Reduce it down to a set
+    num_nans = count_nans(unique_data) # Should be 1 or 2 (NoneType or NaN)
 
-    new_unique = []
-    for dat in unique_data:
-        if isinstance(dat, str):
-            new_unique.append(dat)
-            continue
-        if not math.isnan(dat):
-            new_unique.append(dat)
-    if num_nans > 0:
-        new_unique = append_one_nan(unique_data, new_unique)
+    unique_data_ex_none = []
+    unique_data_nulls = list(pd.isnull(unique_data))
+    for i in range(len(unique_data)):
+        is_null = unique_data_nulls[i]
+        if not is_null:
+            unique_data_ex_none.append(unique_data[i])
 
-    new_unique = move_to_end(new_unique)
-    unique_data_ex_none = list(filter(lambda x: x is not None, new_unique))
+    new_unique = unique_data_ex_none + [None]
     numbered_labels = [i for i in range(len(unique_data_ex_none))]
 
     data_dict = { }
     for name, i in zip(new_unique, numbered_labels):
-        data_dict[name] = float(i)
+        data_dict[name] = int(i)
 
     data_dict[None] = None
 
     return data_dict, numbered_labels
 
 def convert_dataframe(df):
+    """
+    Input: df, with n(i,j)=jth annotation for ith data point
+    Output: new_data, dataframe with inputs updated to numbers indexed from zero
+            numbered_labels, list of integer labels from 0
+            data_dict, dict converting original labels to new integer labels
+    """
     data_dict, numbered_labels = flexible_data(df)
 
     new_data = { }
     for idx, col in df.iteritems():
-        new_column = []
-        for instance in col:
-            if isinstance(instance, str):
-                new_column.append(data_dict[instance])
+        new_col = []
+        isnull_col = list(pd.isnull(col))
+        for i in range(len(col)):
+            isnull = isnull_col[i]
+            if isnull:
+                new_col = new_col + [None]
             else:
-                if math.isnan(instance):
-                    new_column.append(data_dict[None])
-                else:
-                    new_column.append(data_dict[instance])
-        new_data[idx] = new_column
+                new_col.append(data_dict[col[i]])
+        new_data[idx] = new_col
 
     new_data = pd.DataFrame(new_data)
 
